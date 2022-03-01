c6a2850fa3e373d20f6235800a5ec1da61e7fec7
Andrea Timaran <andreaestefania12@hotmail.com>
2022-03-01 16:24:43 -0500

    Added .gitignore

---
+++ getMonthName.js
@@ -0,0 +1,15 @@
+function getMonthName(place){
+    const months = [
+        'january',
+        'february',
+        'march',
+        'april',
+        'may',
+        'june',
+        'july',
+        'august',
+        'september',
+        'october',
+        'november',
+        'december']
+}
